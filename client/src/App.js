3724525a40cc66e6543348100461e70418814d39
KLH7093 <klh7093@gmail.com>
2023-03-14 01:12:43 -0400

    Update the basic layout of the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,41 @@
+import { Routes, Route } from 'react-router-dom';
+import { About } from './components/views/About';
+import { Footer } from './components/views/Footer';
+import { Head } from './components/views/Head.js';
+// import './App.css';
 
-import './App.css';
-import InterestCalculator from './views/InterestCalculator.js';
+import { Home } from './components/views/Home';
+
+
+
+
+
+
 
 
 function App() {
-  return (
+    return (
+    <>
+     <Head/>
 
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+
+
+    </Routes>
     
-    <InterestCalculator/>
-     
-  );
+    
+
+
+     <Footer/>
+
+   </>
+
+    );
 }
 
 export default App;
+
+
+{/* <Route path='*' element={<h1>404</h1>} /> */}
---
+++ client/src/components/views/Footer.js
@@ -0,0 +1,32 @@
+export function Footer(){
+
+    return(
+        <div className="footer_section layout_padding">
+
+
+        <div className="container">
+
+
+    
+ 
+          <div className="copyright_section">
+            <div className="copyright_text">
+              Copyright 2019 All Right Reserved By{" "}
+              <a href="https://html.design">Free html Templates</a>
+            </div>
+          </div>
+       
+
+
+
+
+
+        </div>
+    
+        
+      </div>
+
+
+
+    )
+}
