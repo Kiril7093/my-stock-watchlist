10f7fe13669418063db48c3fa27eb4c97c4655d1
KLH7093 <klh7093@gmail.com>
2023-04-18 05:00:44 -0400

    Added multiple time frame visualization in Details chart
    Updated the Details chart in the stock application to include multiple time frame visualization options.
    Users can now select from 1D, 1W, 1M, 1Y, 1W_1H, and 5Y time frames using a dropdown menu.
    The chart now displays candlestick and volume data for the selected time frame, as well as a moving average line for improved aesthetics.
    Fine-tuned the chart styling by making the moving average line thin and transparent.
    This enhancement improves the user experience by providing a more comprehensive view of historical stock data.

--- eview-app/src/components/Details/DetailsChart.js
+++ eview-app/src/components/Details/DetailsChart.js
@@ -1,69 +1,100 @@
 import React, { useState, useEffect } from "react";
 import { fetchHistoricalData } from "../../api/stockApi";
 
-import { VictoryChart, VictoryCandlestick, VictoryTheme } from "victory";
+import { VictoryChart, VictoryCandlestick, VictoryTheme, VictoryLine } from "victory";
+
+
+
+
 
 export const DetailsChart = ({ symbol }) => {
-  const [watchlistHistoricalData, setWatchlistHistoricalData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const chartConfigKey = "1Y";
-      const data = await fetchHistoricalData(symbol, chartConfigKey);
-
-      setWatchlistHistoricalData(data);
-    };
-    fetchData();
-  }, [symbol]);
-
-  // Extract relevant data from oneWeekHistoricalData oneweek
-  let chartData = null;
-  if (watchlistHistoricalData) {
-    const { c, h, l, o, t, v } = watchlistHistoricalData;
-    chartData = t.map((timestamp, index) => {
-      return {
-        x: new Date(timestamp * 1000),
-        open: o[index],
-        high: h[index],
-        low: l[index],
-        close: c[index],
-        volume: v[index],
+    const [watchlistHistoricalData, setWatchlistHistoricalData] = useState(null);
+    const [chartConfigKey, setChartConfigKey] = useState("1Y");
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        const data = await fetchHistoricalData(symbol, chartConfigKey);
+        setWatchlistHistoricalData(data);
       };
-    });
-  }
-
-  return (
-    <>
-
-{chartData && (
-    <h1 style={{ textAlign: "center", fontSize: "3rem", fontWeight: "bold" }}>{symbol}</h1>
-)}
-    
-      {chartData && (
-         <div >
-        <VictoryChart
-          style={{
-            parent: {
-              border: "1px solid white",
-              backgroundColor: "white", // add background color here
-            },
-          }}
-        >
-          <VictoryCandlestick
-            candleColors={{ positive: "#00b894", negative: "#d63031" }}
-            style={{
-              data: {
-                stroke: "black",
-                strokeWidth: 0.4,
-              },
-            }}
-            data={chartData}
-            candleWidth={3.3}
-            candleRatio={0.1}
-          />
-        </VictoryChart>
+      fetchData();
+    }, [symbol, chartConfigKey]);
+  
+    let chartData = null;
+    if (watchlistHistoricalData) {
+      const { c, h, l, o, t, v } = watchlistHistoricalData;
+      const dates = t.map((timestamp) => new Date(timestamp * 1000));
+      chartData = t.map((timestamp, index) => {
+        return {
+          x: dates[index],
+          open: o[index],
+          high: h[index],
+          low: l[index],
+          close: c[index],
+          volume: v[index],
+        };
+      });
+    }
+  
+    const handleTimeframeChange = (e) => {
+      setChartConfigKey(e.target.value);
+    };
+  
+    return (
+      <>
+        <div>
+          <select value={chartConfigKey} onChange={handleTimeframeChange}>
+            <option value="1D">1 Day</option>
+            <option value="1W">1 Week</option>
+            <option value="1M">1 Month</option>
+            <option value="1Y">1 Year</option>
+            <option value="1W_1H">1 Week (1 Hour)</option>
+            <option value="5Y">5 Years</option>
+          </select>
         </div>
-      )}
-    </>
-  );
-};
+        {chartData && (
+          <h1 style={{ textAlign: "center", fontSize: "3rem", fontWeight: "bold" }}>
+            {symbol}
+          </h1>
+        )}
+        {chartData && (
+          <div>
+            <VictoryChart
+              scale={{ x: "continuous" }}
+              style={{
+                parent: {
+                  border: "1px solid white",
+                  backgroundColor: "white",
+                },
+              }}
+            >
+              <VictoryLine
+                style={{
+                  data: {
+                    stroke: "blue",
+                    strokeWidth: 1,
+                    strokeOpacity: 0.5,
+                  },
+                }}
+                data={chartData}
+                x="x"
+                y="close"
+              />
+              <VictoryCandlestick
+                candleColors={{ positive: "#00b894", negative: "#d63031" }}
+                style={{
+                  data: {
+                    stroke: "black",
+                    strokeWidth: 0.4,
+                  },
+                }}
+                data={chartData}
+                candleWidth={3.3}
+                candleRatio={0.1}
+              />
+            </VictoryChart>
+          </div>
+        )}
+      </>
+    );
+  };
+  
--- eview-app/src/components/WatchList/WatchList.js
+++ eview-app/src/components/WatchList/WatchList.js
@@ -1,25 +1,21 @@
 import { useContext, useEffect } from "react";
 import { stockServiceFactory } from "../../services/stockService";
 import WatchListContext from "../../context/WatchlistContext";
-import png4 from "../../asets/freePng4/png4.png"
+
 import { WatchlistCard } from "./WatchlistCard";
-import { AuthContext } from '../../context/AuthContext'
-
-
-
+import { AuthContext } from "../../context/AuthContext";
 
 export const WatchList = () => {
-  const { watchlistArray, setWatchlistArray, watchlistError, setWatchlistError } =
-    useContext(WatchListContext);
+  const {
+    watchlistArray,
+    setWatchlistArray,
+    watchlistError,
+    setWatchlistError,
+  } = useContext(WatchListContext);
 
+  const { userId, token } = useContext(AuthContext);
 
- const { userId, token } = useContext(AuthContext);
-
- const stockService = stockServiceFactory(token, userId);
-
-
-
-
+  const stockService = stockServiceFactory(token, userId);
 
   useEffect(() => {
     const fetchWatchlist = async () => {
@@ -33,48 +29,42 @@ export const WatchList = () => {
     fetchWatchlist();
   }, []);
 
+  const deleteWatchlistRecord = async (id) => {
+    const deletedStock = await stockService.delete(id);
 
-  
-   
-
-  const deleteWatchlistRecord= async (id)=>{
-
-    
-    const deletedStock= await stockService.delete(id);
-
-    const newArray = watchlistArray.filter(a=>a._id!==id);
+    const newArray = watchlistArray.filter((a) => a._id !== id);
 
     setWatchlistArray(newArray);
-
-
-  
-  }
-
+  };
 
   return (
-    <div className='flex flex-col h-screen bg-zinc-200 mt-10 pt-10'>
-      <h1 className="text-3xl font-bold mb-2 mt-10 pt-10 self-center">My Watchlist</h1>
-          
-
-           {watchlistArray.length===0&&(
-
-              <img src={png4} className="w-90%" alt="/" />
-           )}
+    <div className="flex flex-col h-screen bg-zinc-200 mt-10 pt-10">
+      <h1 className="text-3xl font-bold mb-2 mt-10 pt-10 self-center">
+        My Watchlist
+      </h1>
 
       <div className="flex items-center justify-center flex-1">
-        <div className="flex flex-wrap justify-center gap-4" style={{ maxHeight: "calc(100vh - 300px)", overflowY: "auto" }}>
-          {watchlistArray.map((stock) => (
-            stock._ownerId === userId && (
-              <div
-                key={stock.symbol}
-                className="px-4 py-2 bg-white rounded-md shadow-md"
-              >
-
-   
-                {<WatchlistCard symbol={stock.symbol} data-id={stock._id} deleteWatchlistRecord={deleteWatchlistRecord} />  }
-              </div>
-            )
-          ))}
+        <div
+          className="flex flex-wrap justify-center gap-4"
+          style={{ maxHeight: "calc(100vh - 300px)", overflowY: "auto" }}
+        >
+          {watchlistArray.map(
+            (stock) =>
+              stock._ownerId === userId && (
+                <div
+                  key={stock.symbol}
+                  className="px-4 py-2 bg-white rounded-md shadow-md"
+                >
+                  {
+                    <WatchlistCard
+                      symbol={stock.symbol}
+                      data-id={stock._id}
+                      deleteWatchlistRecord={deleteWatchlistRecord}
+                    />
+                  }
+                </div>
+              )
+          )}
         </div>
       </div>
     </div>
