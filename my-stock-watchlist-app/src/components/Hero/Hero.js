24dd602b9d2ae974b626f2c86a6049ee598f2c25
KLH7093 <klh7093@gmail.com>
2023-05-30 17:13:53 -0400

    Improved aesthetic of hero section with CSS properties
    This commit improves the aesthetic appeal of the hero section in the app. No functionality changes were made. Using CSS properties, I rearranged elements and made minor text adjustments to enhance the visual layout and readability. These changes solely focus on the app's appearance and do not impact its functionality.

--- my-stock-watchlist-app/src/components/Hero/Hero.js
+++ my-stock-watchlist-app/src/components/Hero/Hero.js
@@ -6,17 +6,30 @@ export const Hero = () => {
   return (
     <div className={`w-full h-screen ${styles.heroBackground}`}>
       <div
-        className={`grid md:grid-cols-2 max-w-[1240px] m-auto mb-7px ${styles.heroContent}`}
+        className={`grid md:grid-cols-2 max-w-[1240px] m-auto mb-8px ${styles.heroContent}`}
       >
+
+
         <div
-          className={`flex flex-col justify-center md:items-start w-full px-2 py-8 ${styles.heroText}`}
+          className={`flex flex-col md:items-start w-full pt-5 pb-10 ${styles.heroText}`}
         >
-          <p className="text-2xl text-black">
+          <h1 className={`pt-5 pb-10 text-5xl md:text-7xl font-bold text-black whitespace-nowrap ${styles.heroTitle}`}>
+            My Stock Watchlist
+          </h1>
+          <p className={`text-2xl text-black ${styles.heroSubTitle}`}>
             Track stock prices and trends
           </p>
-          <h1 className="py-7 text-5xl md:text-7xl font-bold text-black">My stock watchlist</h1>
-          <p className="text-lg text-black py-7 my-7">
-          By using my stock watchlist, you can easily monitor your favorite stocks and stay up-to-date on market trends. Keeping track of your investments is a breeze with My stock watchlist.</p>
+
+
+
+          <p className={`text-lg text-black py-3 my-3 ${styles.heroExplanation}`}>
+            {
+            `Monitor your favorite stocks and stay up-to-date on market trends`
+            }
+          
+          </p>
+
+
 
           <button
             className={`py-3 px-6 sm:w-[30%] my-4 bg-indigo-700 ${styles.heroButton} hover:bg-indigo-500`}
@@ -25,11 +38,13 @@ export const Hero = () => {
               Get Started
             </Link>
           </button>
-
-          
         </div>
+
+
+
+
         <div
-          className={`r object-right-top w-50 h-50 ${styles.heroImage}`}
+          className={`object-right-top w-50 h-50 ${styles.heroImage}`}
           style={{ justifySelf: "end" }}
         >
           <img src={pic1} className="w-full" alt="/" />
